661b0d4c358c2340304d26e13850f518da0fa7bd
WilsonWann <genius19880825@hotmail.com>
2023-01-13 10:39:57 +0800

    add GetEditorAction.js

--- src/views/IEditor/IEditor.jsx
+++ src/views/IEditor/IEditor.jsx
@@ -1,4 +1,4 @@
-import React from "react"; // useState
+import React, { useState, useEffect } from "react"; // useState
 import { CKEditor, CKEditorContext } from "@ckeditor/ckeditor5-react";
 // import Editor from "ckeditor5-custom-build/build/ckeditor";
 
@@ -16,29 +16,38 @@ const editorConfiguration = {
 
 function IEditor({ ...props }) {
   //   const [text, setText] = useState("I am editor!!!");
+
+  const [data, setData] = useState("<p>Hello from CKEditor 5!</p>");
+
+//   console.group('IEditorv useEffect data')
+//   console.log(`IEditor data: ${data}`)
+//   console.groupEnd('IEditorv useEffect data')
+  useEffect(() => {
+  }, []);
   return (
     <div className="App">
-        <h2>Using CKEditor 5 build in React</h2>
-        <CKEditor
-            editor={ ClassicEditor }
-            data="<p>Hello from CKEditor 5!</p>"
-            onReady={ editor => {
-                // You can store the "editor" and use when it is needed.
-                console.log( 'Editor is ready to use!', editor );
-            } }
-            onChange={ ( event, editor ) => {
-                const data = editor.getData();
-                console.log( { event, editor, data } );
-            } }
-            onBlur={ ( event, editor ) => {
-                console.log( 'Blur.', editor );
-            } }
-            onFocus={ ( event, editor ) => {
-                console.log( 'Focus.', editor );
-            } }
-        />
+      <h2>Using CKEditor 5 build in React</h2>
+      <CKEditor
+        editor={ClassicEditor}
+        data={data}
+        onReady={(editor) => {
+          // You can store the "editor" and use when it is needed.
+        //   console.log("Editor is ready to use!", editor);
+        }}
+        onChange={(event, editor) => {
+          const data = editor.getData();
+          setData(data);
+          console.log({ event, editor, data });
+        }}
+        onBlur={(event, editor) => {
+        //   console.log("Blur.", editor);
+        }}
+        onFocus={(event, editor) => {
+        //   console.log("Focus.", editor);
+        }}
+      />
     </div>
-);
+  );
 }
 
 export default IEditor;

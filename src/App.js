4892f57cff36329f19761b3c4a70000f3259e122
musense <musense.ad@gmail.com>
2023-05-19 15:39:00 +0800

    App.js setting

--- src/App.js
+++ src/App.js
@@ -1,47 +1,36 @@
 import React, { useEffect } from 'react'
-import { Routes, Route, useNavigate, Link } from 'react-router-dom'
+import { Routes, Route, useNavigate } from 'react-router-dom'
 
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 
-import * as GetEditorAction from "actions/GetEditorAction.js";
 import * as GetClassAction from "actions/GetClassAction.js";
 import * as GetTagsAction from "actions/GetTagsAction.js";
 
 import Admin from "layouts/Admin.jsx";
 import Auth from "layouts/Auth.jsx";
 import Dashboard from 'views/Dashboard/Dashboard.jsx';
-import TableList from 'views/TableList/TableList.jsx'
 import EditorList from 'views/EditorList'
 import IEditor from 'views/IEditor/[id]'
 import NewIEditor from 'views/IEditor'
-import UserProfile from 'views/UserProfile/UserProfile';
 import LoginPage from 'views/Pages/LoginPage';
 import RegisterPage from 'views/Pages/RegisterPage';
 import TagList from './views/TagList';
 import EditorClassList from './views/EditorClassList';
-import EditorManager from './views/EditorManager';
-import EditorManagerNews from "./views/EditorManager/EditorManagerNews";
-import EditorManagerHot from "./views/EditorManager/EditorManagerHot";
-import EditorManagerRecommend from "./views/EditorManager/EditorManagerRecommend";
 import './app.css'
 
+import Cookie from "js-cookie";
 
 function App() {
-    const dispatch = useDispatch()
-
     const navigate = useNavigate()
+    useEffect(() => {     
 
-
-    useEffect(() => {
-        // dispatch({ type: GetClassAction.REQUEST_CLASS_LIST })
+        navigate('/auth/login-page')
+        // navigate('/admin/editorClassList')
         // navigate('/auth/register-page')
-        // navigate('/auth/login-page')
         // navigate('/admin/editorList')
         // navigate('/admin/tag')
-        navigate('/admin/editorClassList')
         // navigate('/admin/table')
         // navigate('/admin/editorList/new')
-        
         // navigate('/admin/editorManager')
     }, []);
 
@@ -50,9 +39,9 @@ function App() {
             <div className='App'>
                 <Routes>
                     <Route path="admin" element={<Admin />} >
-                        <Route path="dashboard" element={<Dashboard />} />
+                        {/* <Route path="dashboard" element={<Dashboard />} /> */}
                         {/* <Route path="table" element={<TableList />} /> */}
-                        <Route path="user" element={<UserProfile />} />
+                        {/* <Route path="user" element={<UserProfile />} /> */}
                         <Route path="tag" element={<TagList />} />
                         <Route path="editorClassList" element={<EditorClassList />} />
 

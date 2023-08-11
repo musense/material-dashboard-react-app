edf729ac850922a3d8ac00ae9018ab7b77f527f3
musense <musense.ad@gmail.com>
2023-08-11 14:37:38 +0800

    將menu移至menu.js

--- src/components/SlateEditor/TableContextMenu/TableContextMenu.jsx
+++ src/components/SlateEditor/TableContextMenu/TableContextMenu.jsx
@@ -5,94 +5,13 @@ import './styles.css'
 import { TableUtil } from '../utils/TableUtil'
 import { Transforms } from 'slate';
 import { ReactEditor } from 'slate-react';
+import menu from './menu.js'
 
 const TableContextMenu = (props) => {
     const { editor } = props;
     const [selection, setSelection] = useState()
     const [showMenu, { top, left }] = useContextMenu(editor, 'table', setSelection);
     const table = new TableUtil(editor);
-
-
-    const menu = [
-        {
-            icon: 'tableToLeft',
-            text: '表格置左',
-            action: {
-                type: 'justify',
-                alignment: 'left'
-            }
-        },
-        {
-            icon: 'tableToCenter',
-            text: '表格置中',
-            action: {
-                type: 'justify',
-                alignment: 'center'
-            }
-        },
-        {
-            icon: 'tableToRight',
-            text: '表格置右',
-            action: {
-                type: 'justify',
-                alignment: 'right'
-            }
-        },
-        {
-            icon: 'insertColumnRight',
-            text: '右側插入一欄',
-            action: {
-                type: 'insertColumn',
-                position: 'after'
-            }
-        },
-        {
-            icon: 'insertColumnLeft',
-            text: '左側插入一欄',
-            action: {
-                type: 'insertColumn',
-                position: 'at'
-            }
-        },
-        {
-            icon: 'insertRowAbove',
-            text: '上面插入一列',
-            action: {
-                type: 'insertRow',
-                position: 'at'
-            }
-        },
-        {
-            icon: 'insertRowBelow',
-            text: '下面插入一列',
-            action: {
-                type: 'insertRow',
-                position: 'after'
-            }
-        },
-        {
-            icon: 'deleteRow',
-            text: '刪除列',
-            action: {
-                type: 'deleteRow'
-            }
-        },
-        {
-            icon: 'deleteColumn',
-            text: '刪除欄',
-            action: {
-                type: 'deleteColumn'
-            }
-        },
-        {
-            icon: 'trashCan',
-            text: '刪除表格',
-            action: {
-                type: 'remove'
-            }
-        }
-    ]
-
 
     const handleInsert = ({ type, position, alignment }) => {
         Transforms.select(editor, selection)

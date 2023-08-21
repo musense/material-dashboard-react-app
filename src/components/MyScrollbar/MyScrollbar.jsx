f8e8eca95497b175e37bda270c41c5aff1865d10
musense <musense.ad@gmail.com>
2023-08-21 17:39:11 +0800

    remove MyScrollbar.module.css

--- src/components/MyScrollbar/MyScrollbar.jsx
+++ src/components/MyScrollbar/MyScrollbar.jsx
@@ -1,20 +1,14 @@
 import React from "react";
 import PerfectScrollBar from 'react-perfect-scrollbar';
 import 'react-perfect-scrollbar/dist/css/styles.css';
-// import 'perfect-scrollbar/css/perfect-scrollbar.css';
-import styles from "./MyScrollbar.module.css"
 
 export default function MyScrollbar({
     children,
     component = 'div',
     height = '',
-    suppressScrollX = null
 }) {
-    console.log("🚀 ~ file: MyScrollbar.jsx:12 ~ suppressScrollX:", suppressScrollX)
     return (
-        <div
-            // className={styles['scrollbar']} 
-            style={{ height: height }}>
+        <div style={{ height: height }}>
             <PerfectScrollBar
                 component={component}
                 suppressScrollX={true}
